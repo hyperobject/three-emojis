4d1018a0af64e447c847dea6285521855191531a
Connor Hudson <cohu3291@colorado.edu>
2018-08-31 22:14:08 -0600

    Add comments and credits

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # three-emojis
-a mastodon bot that posts three emojis per hour
+A [mastodon bot](https://botsin.space/@emojis) that posts three emojis per hour.
+
+Thanks to Srol for [mastodon-ebooks](https://github.com/Srol/mastodon-ebooks), which I stole liberally from, and to http://copy.r74n.com/emojis for an easily-copyable list of All The Emojis. 
--- app.py
+++ app.py
@@ -2,15 +2,15 @@ from mastodon import Mastodon
 import random
 import os
 
-mastodon = Mastodon(
+mastodon = Mastodon( # Get these by going to the Development section of your Mastodon account settings
     client_id=os.environ['client_id'],
     client_secret=os.environ['client_secret'],
     access_token=os.environ['access_token'],
     api_base_url=os.environ['instance']
 )
 
-emojis = open('emojis.txt','r').read().split('\n')
-message = ' '.join(random.choices(emojis, k=3))
+emojis = open('emojis.txt','r').read().split('\n') # Get all the emojis (newline-separated) from emojis.txt
+message = ' '.join(random.choices(emojis, k=3)) # Pick 3 random emojis (wow!)
 
 print('Posting toot:', message)
-mastodon.toot(message)
+mastodon.toot(message) # Post 'em!
